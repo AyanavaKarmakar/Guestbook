85a1a58b006927c59232c83a2e5524b661db669d
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-19 21:03:13 +0530

    feat: create `messages` component to hold messages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
 import type { NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { trpc } from "../utils/trpc";
+
+const Messages = () => {
+  const { data: messages, isLoading } = trpc.guestbook.getAll.useQuery();
+
+  if (isLoading === true) {
+    return <div>Fetching Messages...</div>;
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      {messages?.map((msg, index) => {
+        return (
+          <div key={index}>
+            <p>{msg.message}</p>
+            <span>- {msg.name}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
@@ -26,10 +48,18 @@ const Home: NextPage = () => {
             <p>Hi, {session.user?.name}!</p>
 
             <button onClick={handleSignOut}>Logout</button>
+
+            <div className="pt-10">
+              <Messages />
+            </div>
           </div>
         ) : (
           <div>
             <button onClick={handleGitHubSignIn}>Login with GitHub</button>
+
+            <div className="pt-10">
+              <Messages />
+            </div>
           </div>
         )}
       </div>
