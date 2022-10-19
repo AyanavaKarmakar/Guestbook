c2aa8397f64f3ac3acabdfff8ebf3448ee300fc2
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-19 11:32:53 +0530

    feat: add signin button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from "next";
+import { signIn } from "next-auth/react";
 
 const Home: NextPage = () => {
+  function handleGitHubSignIn() {
+    signIn("github");
+  }
+
   return (
     <main>
       <h1>Guestbook</h1>
+      <button onClick={handleGitHubSignIn}>Login with GitHub</button>
     </main>
   );
 };
