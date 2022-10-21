89687d0d5c1b5a022096834e44ce41de9893faab
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-21 23:49:57 +0530

    feat: fix icon size

--- src/utils/icons.tsx
+++ src/utils/icons.tsx
@@ -81,7 +81,7 @@ export const UserIcon = () => {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="h-6 w-6"
+      className="h-32 w-32"
     >
       <path
         strokeLinecap="round"
